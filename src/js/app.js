65f73a28c46f093b4fae61fc2e5d38f7ac50b35b
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-10-28 01:27:40 +0300

    add clicked button, counter of misses

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,15 @@
 const startGame = document.querySelector('.start_game');
 const stopGame = document.querySelector('.stop_game');
 const hole = [...document.querySelectorAll('.hole')];
+const dead = document.querySelector('.dead');
+const lost = document.querySelector('.lost');
 
 let playing = false;
+let activeHole = 0;
 
 startGame.addEventListener('click', () => {
   playing = true;
-  let activeHole = 0;
+  const getHole = (index) => document.getElementById(`hole${index}`);
   const deactivateHole = (index) => {
     hole[index].classList.remove('hole_has_gnome');
   };
@@ -22,9 +25,29 @@ startGame.addEventListener('click', () => {
     activateHole(activeHole);
     next();
     return null;
-  }, 500);
+  }, 1000);
 
   next();
+
+  const reset = function () {
+    lost.innerText = 0;
+    dead.innerText = 0;
+  };
+
+  for (let i = 1; i < 17; i += 1) {
+    const clickedHole = getHole(i);
+    clickedHole.addEventListener('click', (e) => {
+      const clicked = e.currentTarget;
+      if (clicked.classList.contains('hole_has_gnome') === true) {
+        dead.innerText++;
+      } else {
+        lost.innerText++;
+        if (lost.innerText > 5) {
+          reset();
+        }
+      }
+    });
+  }
 });
 
 stopGame.addEventListener('click', () => {
